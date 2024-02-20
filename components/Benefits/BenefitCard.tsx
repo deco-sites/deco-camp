75b51e218a4d55540d96d0fce41cef6c9128fa1f
RamlilsonSilva <ramilson.silva@rocky.ag>
2024-02-20 19:48:06 -0300

    feat(benefitsSection): change flex to grid

--- components/Benefits/BenefitCard.tsx
+++ components/Benefits/BenefitCard.tsx
@@ -1,23 +1,24 @@
 import { ImageWidget } from "apps/admin/widgets.ts";
 
 export interface Props {
-    image: ImageWidget,
-    title: string,
-    subTitle: string,
-    position: number,
-    arrayLength: number,
+  image: ImageWidget;
+  title: string;
+  subTitle: string;
 }
 
-export default function BenefitCard({ image, title, subTitle, position, arrayLength }: Props) {
-    return (
-        <>
-            <div class={`py-6 ${position === arrayLength - 1 ? 'border-b-0' : 'border-b-[1px]'} md:w-[41%] md:border-none lg:w-[25%] lg:border-r-[1px]`}>
-                <img class="mb-4" src={image} width={32} height={32} />
-                
-                <h4 class="text-[#fff] text-bold text-xl">{title}</h4>
-                <h5 class="mt-2 text-[#A1A1AA] text-base">{subTitle}</h5>
-            </div>
-            <hr class={`${position === 0 || position === 2 ? 'w-[1px] bg-white mx-5 h-auto' : ''} hidden md:block lg:hidden`} />
-        </>
-    );
+export default function BenefitCard(
+  { image, title, subTitle }: Props,
+) {
+  return (
+    <>
+      <div
+        class={`py-6 lg:pl-6 lg:py-0`}
+      >
+        <img class="mb-4" src={image} width={32} height={32} />
+
+        <h4 class="text-[#fff] text-bold text-xl">{title}</h4>
+        <h5 class="mt-2 text-[#A1A1AA] text-base">{subTitle}</h5>
+      </div>
+    </>
+  );
 }
--- components/Benefits/Benefits.tsx
+++ components/Benefits/Benefits.tsx
@@ -1,21 +1,25 @@
 import { ImageWidget } from "apps/admin/widgets.ts";
 
-import BenefitCard from "./BenefitCard.tsx"
+import BenefitCard from "./BenefitCard.tsx";
 
 export interface Props {
-    items: {
-        icon: ImageWidget,
-        title: string,
-        subTitle: string
-    }[]
+  items: {
+    icon: ImageWidget;
+    title: string;
+    subTitle: string;
+  }[];
 }
 
 export default function Benefits({ items }: Props) {
-    return (
-        <div class="container md:flex md:flex-wrap md:gap-6 px-12 lg:justify-between lg:flex-nowrap divide-x-2">
-            {
-                items.map((item, index) => <BenefitCard image={item.icon} title={item.title} subTitle={item.subTitle} position={index} arrayLength={items.length} />)
-            }
-        </div>
-    );
+  return (
+    <div class="container md:grid md:grid-cols-2 md:gap-6 px-12 lg:grid-cols-4 divide-y md:divide-y-0 lg:divide-x">
+      {items.map((item, index) => (
+        <BenefitCard
+          image={item.icon}
+          title={item.title}
+          subTitle={item.subTitle}
+        />
+      ))}
+    </div>
+  );
 }
