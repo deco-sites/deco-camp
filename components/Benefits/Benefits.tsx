49dda432b0c9060583823958cd0adc895ce6ec30
Ramilsons <ramilsonfelixsilva9@gmail.com>
2024-02-21 17:20:23 -0300

    fix(default value): add base props

--- components/Benefits/Benefits.tsx
+++ components/Benefits/Benefits.tsx
@@ -10,7 +10,34 @@ export interface Props {
   }[];
 }
 
-export default function Benefits({ items }: Props) {
+const BASE_PROPS = {
+  items: [
+    {
+      icon: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4391/22c5637f-a952-4fe8-8c22-f2cd53f14ec4",
+      title: "TypeScript, JSX, Tailwind ",
+      subTitle: "Stack we will use"
+    },
+    {
+      icon: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4391/a9b63e44-031c-4aa7-8349-d6f786d97e3c",
+      title: "Intermediate",
+      subTitle: "Recommended Experience"
+    },
+    {
+      icon: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4391/66141bbe-ebde-410e-b575-2916c870c7fb",
+      title: "15h of video content",
+      subTitle: "3 weeks, 5h per week"
+    },
+    {
+      icon: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4391/c3ba4303-cec5-4911-aaf8-66e959ee0996",
+      title: "Flexible Scheduele",
+      subTitle: "Learn in your pace"
+    }
+  ]
+}
+
+export default function Benefits({ props }: { props: Props}) {
+  const { items } = {...BASE_PROPS, ...props};
+
   return (
     <div class="container md:grid md:grid-cols-2 md:gap-6 px-12 lg:grid-cols-4 divide-y md:divide-y-0 lg:divide-x">
       {items.map((item, index) => (
