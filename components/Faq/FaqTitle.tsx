8da84a84d2a3a1a5ce3efd124ea4edf7aa64fabe
Ramilsons <ramilsonfelixsilva9@gmail.com>
2024-02-21 18:57:06 -0300

    refactor(lint): run lint

--- components/Faq/FaqTitle.tsx
+++ components/Faq/FaqTitle.tsx
@@ -1,9 +1,11 @@
 interface Props {
-    title: string
+  title: string;
 }
 
 export default function FaqTitle({ title }: Props) {
-    return (
-        <h2 class="mb-10 text-white font-medium text-[2rem] leading-9 md:text-5xl md:leading-[3.6rem]">{title}</h2>
-    )
+  return (
+    <h2 class="mb-10 text-white font-medium text-[2rem] leading-9 md:text-5xl md:leading-[3.6rem]">
+      {title}
+    </h2>
+  );
 }
