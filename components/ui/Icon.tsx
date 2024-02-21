a8cd6ea649a2d86ce32fada6243d4305443c2033
ApMatheus <matheusaparecido101@gmail.com>
2024-02-21 14:02:01 -0300

    feat:(Mentors) created

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -46,7 +46,10 @@ export type AvailableIcons =
   | "AlertSuccess"
   | "AlertWarning"
   | "AlertError"
-  | "share";
+  | "share"
+  | "Linkedin"
+  | "Git-hub"
+  | "Twitter";
 
 interface Props extends JSX.SVGAttributes<SVGSVGElement> {
   /**
