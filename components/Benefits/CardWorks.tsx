9e67387948d290ffcb61d6f7f24f5a9477a65ad5
ApMatheus <matheusaparecido101@gmail.com>
2024-02-22 10:55:24 -0300

    fix(HowItWorsa) ajustments in background and gap

--- components/Benefits/CardWorks.tsx
+++ components/Benefits/CardWorks.tsx
@@ -21,17 +21,17 @@ export default function CardWorks({ props }: { props: Props }) {
   const { icon, title, content } = props;
 
   return (
-    <div class="flex flex-col gap-4 px-4 w-full md:w-[calc(50%-2rem)] xl:w-[calc(25%-2rem+8px)]">
+    <div class="flex flex-col gap-4 p-8 md:px-[2.1rem] md:py-11 w-full md:w-[calc(50%-1.5rem)] xl:w-[calc(25%-2rem+8px)] bg-[#000D0D] rounded-3xl">
       <div class="bg-base-200 rounded-full w-[56px] h-[56px] flex justify-center items-center">
         <Image width={24} height={24} src={icon} />
       </div>
       <h3
-        class={"text-neutral font-semibold text-[1.5rem] lg:text-2xl "}
+        class={"text-neutral font-semibold text-[1.5rem] leading-[1.65rem] lg:text-2xl "}
         dangerouslySetInnerHTML={{ __html: title }}
       >
       </h3>
       <span
-        class={"text-base mt-auto"}
+        class={"text-base mt-auto text-white"}
         dangerouslySetInnerHTML={{ __html: content }}
       >
       </span>
