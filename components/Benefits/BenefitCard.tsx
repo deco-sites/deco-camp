f0f2eba6947a995c92a93240eee3bf5fc9059bcd
Ramilsons <ramilsonfelixsilva9@gmail.com>
2024-02-22 12:20:54 -0300

    fix(styles): improve styles

--- components/Benefits/BenefitCard.tsx
+++ components/Benefits/BenefitCard.tsx
@@ -1,5 +1,4 @@
 import { ImageWidget } from "apps/admin/widgets.ts";
-
 export interface Props {
   image: ImageWidget;
   title: string;
@@ -13,14 +12,16 @@ export default function BenefitCard(
   return (
     <>
       <div
-        class={`py-6 lg:pl-6 2xl:pl-14 lg:py-0 md:w-[45%] ${
-          position == 0 ? "pl-0 border-t-transparent" : ""
+        class={`py-6 lg:py-0 md:w-[45%] ${
+          position == 0 ? "pl-0 border-t-transparent lg:border-l-transparent lg:pl-0 2xl:pl-0" : "lg:pl-6 2xl:pl-14"
         } ${
           (position + 1) == 1 || (position + 1) == 2
             ? "md:border-t-0 md:border-t-transparent"
             : "border-t-[#A1A1AA]"
         } lg:w-[100%] ${
-          (position + 1) % 2 == 0 ? "md:flex md:justify-start md:pl-7 lg:block" : ""
+          (position + 1) % 2 == 0
+            ? "md:flex md:justify-start md:pl-7 lg:block"
+            : ""
         }`}
       >
         <div>
